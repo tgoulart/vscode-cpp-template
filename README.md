d362f1f0db71b3f9afb47cc1abb30b26a4165657
Thiago Goulart <>
2020-05-16 13:31:20 -0700

    Code template + VSCode tasks for building, testing and debugging the current file + .gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+A barebones VSCode project to support building, running and testing standalone C++ files.
+
+## How
+1. Create any number of matching `.cpp` + `.in` files in the root folder.
+2. Use the builtin shortcuts in VS Code to build, test and debug the active file.
---
+++ codejam_template.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+void single_case(int caseNo) {
+
+    cout << "Case #" << caseNo << ": ";
+}
+
+int main() {
+    int T = 0;
+    cin >> T;
+    for (int i = 1; i <= T; ++i) {
+        single_case(i);
+    }
+    return 0;
+}
